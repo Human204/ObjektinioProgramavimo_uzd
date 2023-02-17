2ba409c1fd33cd286c990a768842c612390cb086
Human204 <tautvydas.naudziunas@gmail.com>
2023-02-17 10:40:42 +0200

    pradiniai failai

---
+++ MyLib.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::copy;
+using std::cout;
+using std::endl;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::setw;
+using std::string;
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "MyLib.h"
+const int paz_sk = 5;
+struct studentas {
+  string vardas = "", pavarde = "";
+  int *paz = nullptr;
+  int egz{0};
+};
+int main(){
+
+}
